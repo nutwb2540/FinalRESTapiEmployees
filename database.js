655d57e8cdbefd24ef74e15b2ab24bcb98b3e2ce
Wachirawit Bumrungchua <nutautomic@gmail.com>
2018-12-15 14:35:10 +0700

    db server

--- database.js
+++ database.js
@@ -1,2 +1,37 @@
 const pgp = require('pg-promise')();
-var db = pgp('');
+var db = pgp('postgres://mqvvplpgwkwaww:bbe283a5d4ce86d696c0794bfca51f66630f8553225b83bff3880b169880250a@ec2-54-243-147-162.compute-1.amazonaws.com:5432/ddhq6akm6qbbtp?ssl=true');
+
+module.exports = {
+    getAllEmployees,
+    getEmployeesByID
+}
+
+function getAllEmployees(req, res) {
+    db.any('select * from employees')
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: data,
+                    message: 'Retrieved ALL employees'
+                });
+        })
+        .catch(function (error) {
+            console.log('ERROR:', error)
+        })
+}
+
+function getEmployeesByID(req, res) {
+    db.any('select * from employees where employee_id =' + req.params.id)
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: data,
+                    message: 'Retrieved employees id:' + req.params.id
+                });
+        })
+        .catch(function (error) {
+            console.log('ERROR:', error)
+        })
+}
---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var db = require('./database');
+
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/', function (req, res) {
+    res.send('Express is running');
+});
+var output = {
+    status: 'success',
+    message: 'REST API is working'
+}
+app.get('/api/json',function(req,res){
+    res.status(500).json(output);
+});
+
+app.get('/api/employees/', db.getAllEmployees);
+app.get('/api/employees/:id', db.getEmployeesByID);
+
+var port = process.env.PORT || 8080;
+app.listen(port, function () {
+    console.log('App is running on http://localhost:' + port);
+});
